e3fbdf9aa355f1d972a6506d7807478402d8883f
Guruprasad-Shrinivas <prasad.pachuu@gmail.com>
2024-01-17 01:18:20 +0530

    17 Jan 2024

---
+++ src/test/java/org/example/vwo/E2E/E2ETest.java
@@ -0,0 +1,17 @@
+package org.example.vwo.E2E;
+
+public class E2ETest {
+
+//Test -> login and verify the dashboard for other widgets
+//positive negative whichever you want
+//Run specific -> Smoke -> Grouping TestNG - Chnage testNG xml
+//Priority - 1,2,2,3,3,,100
+//Add Screenshot - Allure.Takescreen -?
+//on failure you want to do ITestleastener -> Rest Assured
+//Run your code on Jenkins
+//Selenium Grid?
+//Distributed docker
+//AWS machine you can use
+
+    
+}
